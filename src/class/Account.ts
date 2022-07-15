4e9dd8561ee4e945fbcb76a030ca1ee07e2e16fc
rokas <rokasra@gmail.com>
2022-07-15 09:28:31 +0300

    owner uzd

---
+++ src/class/Account.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/lines-between-class-members */
+/* eslint-disable no-underscore-dangle */
+// OOP
+class Account {
+  // properties
+  // public - default, access from anywere
+  // private - access from whitnin class
+  // protected - access from within and inherited classes
+
+  username?: string | undefined;
+  // paraterer properties
+  constructor(public readonly id: number, public owner: string, private _balance: number) {}
+
+  deposit(value: number): void {
+    if (value < 0) throw new Error('Invalid amount');
+    // record transaction
+    this._balance += value;
+  }
+
+  private calculateTax(tax: number): void {
+    console.log(this._balance * tax);
+  }
+
+  getBalace(): number {
+    return this._balance;
+  }
+}
+
+const acc = new Account(1, 'Serbentautas', 5);
+acc.username = 'Serbas';
+// acc.id = 5; // error
+console.log('acc.getBalace() ===', acc.getBalace());
+const acc2 = new Account(2, 'James Bond', 500);
+acc2.deposit(500);
+// padaryti owner nepasiekiama is isores
+acc2.owner = 'MIke';
+
+// pasirasom metoda kad gautu ownerio varda
+
+console.log('acc ===', acc);
+console.log('acc2 ===', acc2);
