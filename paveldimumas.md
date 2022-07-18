a6eca7e93d3443a1eb09f1e3e5d60df27a2029e3
rokas <rokasra@gmail.com>
2022-07-18 11:17:10 +0300

    uzd calc

--- paveldimumas.md
+++ paveldimumas.md
@@ -4,5 +4,22 @@
 2. firstName, lastName,
 3. fullName getteris = grazina 'James Bond'
 4. walk metodas paduodam sk kilomentrais ir atspausdina kad nuejom xx kilometru
-5. prideti is isores neprieinama savybe todayWalked. Sukurimo metu ji lygi 0. Ji sekas kiek zmogus nuejo per diena. Reiskia kiekviena karta kai zmogus eina kazkiek km tie km prisideda prie todayWalked.
+5. prideti is isores neprieinama savybe todayWalked. Sukurimo metu ji lygi 0. Ji seka kiek zmogus nuejo per diena. Reiskia kiekviena karta kai zmogus eina kazkiek km tie km prisideda prie todayWalked.
    5.1 sukurti geteri pamatyti kiek nuejom. Getteris truretu grazinti skaiciu km. ir isspausdinti 'James walked for 10km today'
+
+## paveld
+
+1. Sukurti Teacher clase kuri paveldi Person
+2. prideti Subject gali buti 'Math', 'Literature', 'Architecture'
+3. Teacher klasei prideti workWeek savybe kuri yra skaiciu masyvas ir zymi kuriomis dienomis jis dirba. pvz [1, 3, 5] reiskia pirmadieni treciadini, penktadieni
+4. sukurti getteri kuris grazina stringo pavidalu kuriom dienom dirbama, pvz [1, 3, 5] => 'pirmadienis, treciadinis, penktadienis'.
+5. sukurti 2 mokytojus ir padaryti kad jie nueitu po 20km per 2-3 kartus.
+6. nustatyti workWeek mokytojui kad dirbtu sestadieni sekmadieni. paziureti ar veikia
+
+## Calculator
+
+1. sukurti Calc clase. Ji priima 2 skaitinius argumentus.
+2. aprasyti 'add', 'minus', 'divide', 'multiply' metodus kurie atlieka isvardintus veiksmus, atspausdina ir grazina rezultata.
+3. sukurti ScientificCalc clase kuri paveldi Calc clase.
+4. prideti papildomai 'pliusProc', 'minusProc', 'power'(kelti laipsniu) veiksmus.
+5. sukurti buda irasyti visas atliktas operacijas su skaiciuotuvu nuo jo sukurimo su 'new'
--- src/class/Person.ts
+++ src/class/Person.ts
@@ -1,6 +1,95 @@
+/* eslint-disable no-sparse-arrays */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable @typescript-eslint/lines-between-class-members */
+/* eslint-disable no-underscore-dangle */
 // sukurti klase Person
-// firstName, lastName
-// fullName getteris = grazina 'James Bond'
-// walk metodas paduodam sk kilometrais ir atspauzdina kad nuejom xx kilometru
 
-interface PersonInterface {}
+class Person {
+  // firstName, lastName,
+  private _todayWalked: number;
+  constructor(public firstName: string, public lastName: string) {
+    this._todayWalked = 0;
+  }
+
+  // getter
+  get fullName(): string {
+    // fullName getteris = grazina 'James Bond'
+    return `${this.firstName} ${this.lastName}!`;
+  }
+
+  // walk
+  // walk metodas paduodam sk kilomentrais ir atspausdina kad nuejom xx kilometru
+  walk(val: number): void {
+    this._todayWalked += val;
+    console.log(`${this.firstName} walked ${val} km just now. Yeyyy!`);
+  }
+
+  // getteris today waked
+  get todayWalked(): number {
+    console.log(`${this.firstName} walked ${this._todayWalked} km Today. Yeyyy!`);
+    return this._todayWalked;
+  }
+}
+
+class Student extends Person {
+  constructor(public readonly email: string, firstName: string, lastName: string) {
+    // super === Person.contstrutor()
+    super(firstName, lastName);
+  }
+
+  takeTest(subject: string): void {
+    console.log(`${this.firstName} is taking a test in ${subject}`);
+  }
+}
+
+type TeacherSubs = 'Math' | 'Literature' | 'Architecture';
+
+class Teacher extends Person {
+  workWeek: [1?, 2?, 3?, 4?, 5?, 6?, 7?] = [];
+  constructor(public subject: TeacherSubs, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+  // perrasant tevines klases metoda pridedam override
+  override get fullName(): string {
+    // super === tevine klase
+    return `Doc. ${super.fullName}`;
+  }
+}
+
+const t1 = new Teacher('Architecture', 'James', 'Band');
+const t2 = new Teacher('Math', 'Mike', 'Tyson');
+// t2.walk(5);
+// t2.walk(5);
+// t2.walk(10);
+// [7, 7, 6].forEach((t: number) => t1.walk(t));
+// console.log('t1 ===', t1.todayWalked);
+// console.log('t2 ===', t2.todayWalked);
+
+t1.workWeek = [1, 2, , , 5];
+
+console.log('t1.fullName ===', t1.fullName);
+
+// const p1 = new Person('Severijus', 'Klaida');
+// const p2 = new Student('Sebas@Niekada.com', 'Sebentautas', 'Niekada');
+// p2.takeTest('Node authorization');
+// // console.log('p2 ===', p2);
+// // console.log(p1.fullName);
+// // p1.walk(5);
+// // p1.walk(5);
+// p2.walk(5);
+// p2.walk(7);
+// // cp1onsole.log('p1 ===', p1);
+// p2.todayWalked;
+// p1.fullName = 'Severijus';
+
+// extends ekvivalentas
+// const user = {
+//   name: 'John',
+//   age: 34,
+// };
+// const userEmail = {
+//   ...user,
+//   email: 'john@example.com',
+// };
